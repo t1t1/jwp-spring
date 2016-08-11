9b2853e5f624e5109e3ff111661b78f416e19895
t1t1 <cha@hanmail2.net>
2016-08-11 19:50:41 +0900

    Environment -> @Value("${db.driver}"

--- src/main/java/next/config/AppConfig.java
+++ src/main/java/next/config/AppConfig.java
@@ -4,11 +4,13 @@ import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
@@ -20,10 +22,20 @@ import org.springframework.stereotype.Controller;
 )
 @PropertySource("classpath:application.properties")
 public class AppConfig {
-	private static final String DB_DRIVER = "org.h2.Driver";
-	private static final String DB_URL = "jdbc:h2:~/jwp-basic;AUTO_SERVER=TRUE";
-	private static final String DB_USERNAME = "sa";
-	private static final String DB_PW = "";
+	
+	@Value("${db.driver}")
+	private String DB_DRIVER;
+	@Value("${db.url}")
+	private String DB_URL;
+	@Value("${db.username}")
+	private String DB_USERNAME;
+	@Value("${db.password}")
+	private String DB_PW;
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 	
 	@Autowired
 	private Environment env;
@@ -31,14 +43,14 @@ public class AppConfig {
 	@Bean
 	public DataSource dataSource() {
 		BasicDataSource ds = new BasicDataSource();
-//		ds.setDriverClassName(DB_DRIVER);
-//		ds.setUrl(DB_URL);
-//		ds.setUsername(DB_USERNAME);
-//		ds.setPassword(DB_PW);
-		ds.setDriverClassName(env.getProperty("db.driver"));
-		ds.setUrl(env.getProperty("db.url"));
-		ds.setUsername(env.getProperty("db.username"));
-		ds.setPassword(env.getProperty("db.password"));
+		ds.setDriverClassName(DB_DRIVER);
+		ds.setUrl(DB_URL);
+		ds.setUsername(DB_USERNAME);
+		ds.setPassword(DB_PW);
+//		ds.setDriverClassName(env.getProperty("db.driver"));
+//		ds.setUrl(env.getProperty("db.url"));
+//		ds.setUsername(env.getProperty("db.username"));
+//		ds.setPassword(env.getProperty("db.password"));
 		return ds;
 	}
 	
